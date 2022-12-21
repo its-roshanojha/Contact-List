df4021b1fc475dc81b673f3ceccce3913ea32074
Roshan <its.roshanojha@gmail.com>
2022-12-21 10:21:24 +0530

    modified navbar little bit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div>
-      <nav className='navbar navbar-expand-lg navbar-dark bg-dark py-2'>
-            <div className='container-fluid'>
-                <Link to='/' className='navbar-brand ml-5'>Contact List</Link>
-            </div>
-        </nav >
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark " >
+  <div class="container-fluid">
+    <Link class="navbar-brand" to={'/'}>Contact List</Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <Link class="nav-link active" aria-current="page" to={'/'}>Home</Link>
+        </li>
+        
+      </ul>
+      
     </div>
-  )
+  </div>
+</nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
