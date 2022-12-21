1f5b9d2ad1e31cfb8d02bf3d645f5a6a83f42acb
Roshan <its.roshanojha@gmail.com>
2022-12-21 11:20:47 +0530

    Changed the position of the addContact button

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,10 +16,8 @@ const Home = () => {
 
     return (
         <div className='container'>
-            <div className='row'>
-                <div className='col-md-12 my-5 text-end'>
-                    <Link to='/add' className='btn btn-outline-dark'>Add Contact</Link>
-                </div>
+            <div className='row my-4'>
+                
                 <div className='col-md-10 mx-auto'>
                     <table className='table table-hover'>
                         <thead className='text-white bg-dark text-center'>
@@ -49,6 +47,7 @@ const Home = () => {
                         </tbody>
                     </table>
                 </div>
+                
             </div>
         </div >
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,25 +4,46 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark " >
-  <div class="container-fluid">
-    <Link class="navbar-brand" to={'/'}>Contact List</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to={'/'}>Home</Link>
-        </li>
-        
-      </ul>
-      
-    </div>
-  </div>
-</nav>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <div class="container-fluid">
+          <Link class="navbar-brand" to={"/"}>
+            Contact List
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link class="nav-link active" aria-current="page" to={"/"}>
+                  Home
+                </Link>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+
+              <Link to='/add' className='btn btn-dark'>Add Contact</Link>
+             
+            </form>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
 
 export default Navbar;
+
+{
+  /* <div className='col-md-2 my-1 text-end'>
+                    <Link to='/add' className='btn btn-outline-dark'>Add Contact</Link>
+                </div> */
+}
