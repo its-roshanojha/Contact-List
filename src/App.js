a7cdc75135ab28287243f84d42fa2075c83b1d1d
Roshan <its.roshanojha@gmail.com>
2022-12-21 09:24:03 +0530

    Created Home page to do CRUD in the contact-list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar/>
+        <Home/>
         <Routes>
 
         </Routes>
---
+++ src/components/Home.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12 my-5 text-end">
+          <Link to="/add" className="btn btn-outline-dark">
+            Add Contact
+          </Link>
+        </div>
+        <div className="col-md-10 mx-auto">
+          <table className="table table-hover">
+            <thead className="text-white bg-dark text-center">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Number</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>id</td>
+                <td>name</td>
+                <td>email</td>
+                <td>number</td>
+                <td>
+                  <Link
+                    className="btn btn-small btn-primary me-2"
+                  >
+                    Edit
+                  </Link>
+                  <button type="button" className="btn btn-small btn-danger">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
